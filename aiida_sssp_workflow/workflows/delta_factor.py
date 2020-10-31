26212b8ae6c07f7e9a660c03c1e981f226184db0
Jason Eu <morty.yu@yahoo.com>
2020-10-31 15:51:00 +0800

    DeltaWC add clean workdir

--- aiida_sssp_workflow/workflows/delta_factor.py
+++ aiida_sssp_workflow/workflows/delta_factor.py
@@ -48,7 +48,8 @@ class DeltaFactorWorkChain(WorkChain):
                    help='Numbers of scale points in eos step.')
         spec.input('parameters.scale_increment', valid_type=orm.Float, default=lambda: orm.Float(0.02),
                    help='The scale increment in eos step.')
-        # TODO clean_workdir
+        spec.input('clean_workdir', valid_type=orm.Bool, default=lambda: orm.Bool(False),
+            help='If `True`, work directories of all called calculation will be cleaned at the end of execution.')
         spec.outline(
             cls.setup,
             cls.validate_structure_and_pseudo,
@@ -146,3 +147,24 @@ class DeltaFactorWorkChain(WorkChain):
         self.out('delta_factor', self.ctx.delta_factor)
         self.out('element', self.ctx.element)
         # TODO output the parameters used for eos and pw.
+
+    def on_terminated(self):
+        """Clean the working directories of all child calculations if `clean_workdir=True` in the inputs."""
+        super().on_terminated()
+
+        if self.inputs.clean_workdir.value is False:
+            self.report('remote folders will not be cleaned')
+            return
+
+        cleaned_calcs = []
+
+        for called_descendant in self.node.called_descendants:
+            if isinstance(called_descendant, orm.CalcJobNode):
+                try:
+                    called_descendant.outputs.remote_folder._clean()  # pylint: disable=protected-access
+                    cleaned_calcs.append(called_descendant.pk)
+                except (IOError, OSError, KeyError):
+                    pass
+
+        if cleaned_calcs:
+            self.report(f"cleaned remote folders of calculations: {' '.join(map(str, cleaned_calcs))}")
