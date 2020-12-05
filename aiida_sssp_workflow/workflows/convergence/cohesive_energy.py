5fb556b23bda5b7ecb6400f929b487a90d76cb8c
Jason Eu <morty.yu@yahoo.com>
2020-12-05 15:59:35 +0800

    genaral cohesive energy evaluation
    When cohesive energy convergence test an F(fluorine) use structure SiF4,
    automatically setting by the convergence workflow. Similar of
    lanthanides, use Re-N.
    
    Things become tricky, when the isolate atom energy of lanthanides is
    going to be calculated. NEEDED TO increase the `nbnds` and use much more
    RAM(by in crease the number of machines).
    
    The inputs parameters of convergence workflow are `ecutwfc_list` and
    `ecutrho_list` with dual concept deprecated.

--- aiida_sssp_workflow/workflows/convergence/cohesive_energy.py
+++ aiida_sssp_workflow/workflows/convergence/cohesive_energy.py
@@ -2,16 +2,28 @@
 """
 Convergence test on cohesive energy of a given pseudopotential
 """
+import importlib_resources
 from aiida.common import AttributeDict
 from aiida.engine import WorkChain, ToContext, append_
 from aiida import orm
 from aiida.plugins import WorkflowFactory
+
+from aiida.common.files import md5_file
+from aiida_sssp_workflow.workflows.delta_factor import helper_parse_upf, \
+    get_standard_cif_filename_from_element, \
+    helper_create_standard_cif_from_element, \
+    RARE_EARTH_ELEMENTS, update_dict
 
 CohesiveEnergyWorkChain = WorkflowFactory('sssp_workflow.cohesive_energy')
 
 PARA_ECUTWFC_LIST = lambda: orm.List(list=[
     20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 110,
     120, 130, 140, 160, 180, 200
+])
+
+PARA_ECUTRHO_LIST = lambda: orm.List(list=[
+    160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600, 640, 680, 720,
+    760, 800, 880, 960, 1040, 1120, 1280, 1440, 1600
 ])
 
 
@@ -27,20 +39,15 @@ class ConvergenceCohesiveEnergyWorkChain(WorkChain):
                    valid_type=orm.UpfData,
                    required=True,
                    help='Pseudopotential to be verified')
-        spec.input(
-            'structure',
-            valid_type=orm.StructureData,
-            required=True,
-            help='Ground state structure which the verification perform')
         spec.input('options',
                    valid_type=orm.Dict,
                    required=False,
                    help='Optional `options` to use for the `PwCalculations`.')
         spec.input_namespace('parameters', help='Para')
-        spec.input('parameters.dual',
-                   valid_type=orm.Int,
-                   default=lambda: orm.Int(8),
-                   help='dual value for ecutwfc/ecutrho pair.')
+        spec.input('parameters.ecutrho_list',
+                   valid_type=orm.List,
+                   default=lambda: PARA_ECUTRHO_LIST,
+                   help='dual value for ecutrho list.')
         spec.input('parameters.ecutwfc_list',
                    valid_type=orm.List,
                    default=PARA_ECUTWFC_LIST,
@@ -67,33 +74,104 @@ class ConvergenceCohesiveEnergyWorkChain(WorkChain):
             400,
             'ERROR_SUB_PROCESS_FAILED',
             message='The sub processes {pk} did not finish successfully.')
+        spec.exit_code(
+            510,
+            'ERROR_DIFFERENT_SIZE_OF_ECUTOFF_PAIRS',
+            message='The ecutwfc_list and ecutrho_list have incompatible size.'
+        )
 
     def setup(self):
-        pass
+        self.ctx.ecutwfc_list = self.inputs.parameters.ecutwfc_list.get_list()
+        self.ctx.ecutrho_list = self.inputs.parameters.ecutrho_list.get_list()
+        if not len(self.ctx.ecutwfc_list) == len(self.ctx.ecutrho_list):
+            return self.exit_codes.ERROR_DIFFERENT_SIZE_OF_ECUTOFF_PAIRS
 
     def validate_structure(self):
-        pass
+        upf_info = helper_parse_upf(self.inputs.pseudo)
+        element = orm.Str(upf_info['element'])
+
+        pseudos = {element.value: self.inputs.pseudo}
+        self.ctx.element = element
+
+        if element.value == 'F':
+            self.ctx.element = orm.Str('SiF4')
+
+            fpath = importlib_resources.path('aiida_sssp_workflow.REF.UPFs',
+                                             'Si.pbe-n-rrkjus_psl.1.0.0.UPF')
+            with fpath as path:
+                filename = str(path)
+                upf_silicon = orm.UpfData.get_or_create(filename)[0]
+                pseudos['Si'] = upf_silicon
+
+        pw_parameters = {}
+        if element.value in RARE_EARTH_ELEMENTS:
+            fpath = importlib_resources.path('aiida_sssp_workflow.REF.UPFs',
+                                             'N.pbe-n-radius_5.UPF')
+            with fpath as path:
+                filename = str(path)
+                upf_nitrogen = orm.UpfData.get_or_create(filename)[0]
+                pseudos['N'] = upf_nitrogen
+
+            z_valence_RE = upf_info['z_valence']  # pylint: disable=invalid-name
+            z_valence_N = helper_parse_upf(upf_nitrogen)['z_valence']  # pylint: disable=invalid-name
+            nbands = (z_valence_N + z_valence_RE) // 2
+            nbands_factor = 2
+            pw_parameters = {
+                'SYSTEM': {
+                    'nbnd': int(nbands * nbands_factor),
+                },
+            }
+
+        filename = get_standard_cif_filename_from_element(
+            self.ctx.element.value)
+
+        md5 = md5_file(filename)
+        cifs = orm.CifData.from_md5(md5)
+        if not cifs:
+            # cif not stored, create it with calcfunction and return it
+            cif_data = helper_create_standard_cif_from_element(
+                self.ctx.element)
+        else:
+            # The Cif is already store let's return it
+            cif_data = orm.CifData.get_or_create(filename)[0]
+
+        self.ctx.structure = cif_data.get_structure()
+        self.ctx.pseudos = pseudos
+        self.ctx.pw_parameters = pw_parameters
 
     def get_inputs(self, ecutwfc, ecutrho):
-        _PW_PARAS = orm.Dict( # pylint: disable=invalid-name
-            dict={
-                'SYSTEM': {
-                    'degauss': 0.01,
-                    'occupations': 'smearing',
-                    'smearing': 'marzari-vanderbilt',
-                    'ecutrho': ecutrho,
-                    'ecutwfc': ecutwfc,
-                },
-            })
+        _PW_BULK_PARAS = {   # pylint: disable=invalid-name
+            'SYSTEM': {
+                'degauss': 0.01,
+                'occupations': 'smearing',
+                'smearing': 'marzari-vanderbilt',
+                'ecutrho': ecutrho,
+                'ecutwfc': ecutwfc,
+            },
+        }
+        _PW_ATOM_PARAS = {   # pylint: disable=invalid-name
+            'SYSTEM': {
+                'degauss': 0.01,
+                'occupations': 'smearing',
+                'smearing': 'gaussian',
+                'ecutrho': ecutrho,
+                'ecutwfc': ecutwfc,
+            },
+        }
         inputs = AttributeDict({
             'code': self.inputs.code,
-            'pseudo': self.inputs.pseudo,
-            'structure': self.inputs.structure,
+            'pseudos': self.ctx.pseudos,
+            'structure': self.ctx.structure,
             'parameters': {
-                'pw_bulk': _PW_PARAS,
-                'pw_atom': _PW_PARAS,
-                'kpoints_distance': orm.Float(0.15),
-                'vacuum_length': orm.Float(12.0),
+                'pw_bulk':
+                orm.Dict(
+                    dict=update_dict(_PW_BULK_PARAS, self.ctx.pw_parameters)),
+                'pw_atom':
+                orm.Dict(dict=_PW_ATOM_PARAS),
+                'kpoints_distance':
+                orm.Float(0.15),
+                'vacuum_length':
+                orm.Float(12.0),
             },
         })
 
@@ -105,7 +183,7 @@ class ConvergenceCohesiveEnergyWorkChain(WorkChain):
         hard code to 200Ry at the moment
         """
         ecutwfc = 200
-        ecutrho = 200 * self.inputs.parameters.dual.value
+        ecutrho = 1600  # TODO if NC set to 800
         inputs = self.get_inputs(ecutwfc, ecutrho)
 
         running = self.submit(CohesiveEnergyWorkChain, **inputs)
@@ -131,8 +209,8 @@ class ConvergenceCohesiveEnergyWorkChain(WorkChain):
         self.ctx.ref_energy = ref_workchain.outputs.output_parameters[
             'cohesive_energy']
 
-        for ecutwfc in self.inputs.parameters.ecutwfc_list:
-            ecutrho = ecutwfc * self.inputs.parameters.dual.value
+        for ecutwfc, ecutrho in zip(self.ctx.ecutwfc_list,
+                                    self.ctx.ecutrho_list):
             inputs = self.get_inputs(ecutwfc, ecutrho)
 
             workchain = self.submit(CohesiveEnergyWorkChain, **inputs)
--- examples/example_cohesive_energy.py
+++ examples/example_cohesive_energy.py
@@ -14,19 +14,29 @@ if __name__ == '__main__':
     code = load_code('qe-6.6-pw@daint-mc')
 
     # # Silicon structure and pseudopotential
-    structure = load_node('0c5793b9-e52c-4708-b5a1-f53b5c4814bf')
-    upf = load_node('9d9d57fc-49e3-4e0c-8c37-4682ccc0fb51')
+    # upf = load_node('9d9d57fc-49e3-4e0c-8c37-4682ccc0fb51')
+
+    # Fluorine pseude
+    # upf = load_node('a1b60d81-d5a3-4ddb-a31a-d5b608c1ba52')
+
+    # gold structure and pseudopotential
+    # upf = load_node('197acb08-ff93-4e65-8de9-2242a96197b2') # NC
+    upf = load_node('61830c2b-fa08-4e92-9571-1141ded79612')  # PAW
+
+    # Lanthanum
+    # upf = load_node('b2880763-579c-4f6d-8803-2c77f4fb10e8')
 
     PARA_ECUTWFC_LIST = orm.List(list=[20, 25, 200])
+    PARA_ECUTRHO_LIST = orm.List(list=[160, 200, 1600])
 
     inputs = AttributeDict({
         'code': code,
         'pseudo': upf,
-        'structure': structure,
         'parameters': {
-            'dual': orm.Int(8),
             'ecutwfc_list': PARA_ECUTWFC_LIST,
+            'ecutrho_list': PARA_ECUTRHO_LIST,
         },
     })
     node = submit(ConvergenceCohesiveEnergy, **inputs)
+    node.description = f'[{upf.element}]'
     print(node)
