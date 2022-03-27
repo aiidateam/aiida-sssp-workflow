1503c81f03f7f11a4d6dff8fe7ec34df99f13c5b
Jusong Yu <jusong.yeu@gmail.com>
2022-03-27 11:10:02 +0200

    Add bands measure workflow

--- aiida_sssp_workflow/workflows/measure/bands.py
+++ aiida_sssp_workflow/workflows/measure/bands.py
@@ -4,18 +4,15 @@ Bands distance of many input pseudos
 """
 import importlib
 
-import yaml
 from aiida import orm
-from aiida.engine import WorkChain, append_, if_
+from aiida.engine import ToContext, WorkChain, if_
 from aiida.plugins import DataFactory
 
-from aiida_sssp_workflow.calculations.calculate_bands_distance import (
-    calculate_bands_distance,
-)
 from aiida_sssp_workflow.utils import (
     NONMETAL_ELEMENTS,
     RARE_EARTH_ELEMENTS,
-    get_cif_abspath,
+    get_protocol,
+    get_standard_structure,
     update_dict,
 )
 from aiida_sssp_workflow.workflows.evaluate._bands import BandsWorkChain
@@ -31,14 +28,14 @@ def validate_input_pseudos(d_pseudos, _):
         return f'The pseudos corespond to different elements {element}.'
 
 
-class BandsDistanceWorkChain(WorkChain):
-    """WorkChain to do bands comparision of different pseudos"""
-
+class BandsMeasureWorkChain(WorkChain):
+    """
+    WorkChain to run bands measure,
+    run without sym for distance compare and band structure along the path
+    """
     _MAX_WALLCLOCK_SECONDS = 1800 * 3
 
     _LARGE_DUAL_ELEMENTS = ['Fe', 'Hf']
-
-    _INIT_NBANDS_FACTOR = 3.0
     _RY_TO_EV = 13.6056980659
 
     @classmethod
@@ -46,13 +43,14 @@ class BandsDistanceWorkChain(WorkChain):
         """Define the process specification."""
         super().define(spec)
         # yapf: disable
-        spec.input('code', valid_type=orm.Code,
+        spec.input('pw_code', valid_type=orm.Code,
                     help='The `pw.x` code use for the `PwCalculation`.')
-        spec.input_namespace('input_pseudos', valid_type=UpfData, dynamic=True,
-                    validator=validate_input_pseudos,
-                    help='A mapping of `UpfData` node to be verified onto file name:upf.')
-        spec.input('protocol', valid_type=orm.Str, default=lambda: orm.Str('theos'),
-                    help='The protocol to use for the workchain.')
+        spec.input('pseudo', valid_type=UpfData, required=True,
+                    help='Pseudopotential to be verified')
+        spec.input('protocol', valid_type=orm.Str, required=True,
+                    help='The protocol which define input calculation parameters.')
+        spec.input('cutoff_control', valid_type=orm.Str, default=lambda: orm.Str('standard'),
+                    help='The control protocol where define max_wfc.')
         spec.input('options', valid_type=orm.Dict, required=False,
                     help='Optional `options` to use for the `PwCalculations`.')
         spec.input('parallelization', valid_type=orm.Dict, required=False,
@@ -64,23 +62,13 @@ class BandsDistanceWorkChain(WorkChain):
             cls.init_setup,
             if_(cls.is_rare_earth_element)(
                 cls.extra_setup_for_rare_earth_element, ),
-            cls.setup_code_parameters_from_protocol,
-            cls.setup_code_resource_options,
+            cls.setup_pw_parameters_from_protocol,
+            cls.setup_pw_resource_options,
             cls.run_bands_evaluation,
-            cls.calculate_bands_distance,
+            cls.finalize,
         )
 
-        spec.output('output_bands_distance', valid_type=orm.Dict, required=True,
-            help='the distance of each pair of pseudopotentials.')
-
-    def _get_protocol(self):
-        """Load and read protocol from faml file to a verbose dict"""
-        import_path = importlib.resources.path('aiida_sssp_workflow',
-                                               'protocol_calc.yml')
-        with import_path as pp_path, open(pp_path, 'rb') as handle:
-            self._protocol = yaml.safe_load(handle)  # pylint: disable=attribute-defined-outside-init
-
-            return self._protocol
+        spec.expose_outputs(BandsWorkChain)
 
     def init_setup(self):
         """
@@ -91,19 +79,12 @@ class BandsDistanceWorkChain(WorkChain):
         self.ctx.pw_parameters = {}
         self.ctx.extra_parameters = {}
 
-        # First idx since already being validate to make sure all mapping to same element
-        element = [pseudo.element for pseudo in self.inputs.input_pseudos.values()][0]
-        self.ctx.element = element
+        element = self.ctx.element = self.inputs.pseudo.element
 
-        # Structures for convergence verification are all primitive structures
-        # the original conventional structure comes from the same CIF files of
-        # http:// molmod.ugent.be/deltacodesdft/
-        # EXCEPT that for the element fluorine the `SiF4.cif` used for convergence
-        # reason. But we do the structure setup for SiF4 in the following step:
-        # `cls.extra_setup_for_fluorine_element`
-        cif_file = get_cif_abspath(element)
-        self.ctx.structure = orm.CifData.get_or_create(
-            cif_file)[0].get_structure(primitive_cell=True)
+        self.ctx.structure = get_standard_structure(
+            element,
+            prop="bands",
+        )
 
         # extra setting for bands convergence
         self.ctx.is_metal = element not in NONMETAL_ELEMENTS
@@ -132,69 +113,72 @@ class BandsDistanceWorkChain(WorkChain):
             },
         }
 
-    def setup_code_parameters_from_protocol(self):
+    def setup_pw_parameters_from_protocol(self):
         """Input validation"""
         # pylint: disable=invalid-name, attribute-defined-outside-init
 
         # Read from protocol if parameters not set from inputs
-        protocol_name = self.inputs.protocol.value
-        protocol = self._get_protocol()[protocol_name]
-        protocol = protocol['bands_distance']
+        protocol = get_protocol(category="bands", name=self.inputs.protocol.value)
         self._DEGAUSS = protocol['degauss']
         self._OCCUPATIONS = protocol['occupations']
         self._SMEARING = protocol['smearing']
         self._CONV_THR = protocol['electron_conv_thr']
         self._KDISTANCE = protocol['kpoints_distance']
 
-        self.ctx.ecutwfc = protocol['ecutwfc']
+        self._INIT_NBANDS_FACTOR = protocol['init_nbands_factor']
+        self._BANDS_SHIFT = protocol['bands_shift']
+
+        cutoff_control = get_protocol(
+            category="control", name=self.inputs.cutoff_control.value
+        )
+        self._ECUTWFC = cutoff_control["max_wfc"]
+
+        self.ctx.ecutwfc = self._ECUTWFC
         self.ctx.kpoints_distance = self._KDISTANCE
-        self.ctx.degauss = self._DEGAUSS
+        self.ctx.init_nbands_factor = self._INIT_NBANDS_FACTOR
+        self.ctx.bands_shift = self._BANDS_SHIFT
 
-        # set large ecutrho for specific elements
-        if self.ctx.element in self._LARGE_DUAL_ELEMENTS:
-            self.ctx.ecutrho = protocol['ecutrho'] * 2
-        else:
-            self.ctx.ecutrho = protocol['ecutrho']
-
-        self.ctx.pw_parameters = {
-            'SYSTEM': {
-                'degauss': self._DEGAUSS,
-                'occupations': self._OCCUPATIONS,
-                'smearing': self._SMEARING,
+        parameters = {
+            "SYSTEM": {
+                "degauss": self._DEGAUSS,
+                "occupations": self._OCCUPATIONS,
+                "smearing": self._SMEARING,
             },
-            'ELECTRONS': {
-                'conv_thr': self._CONV_THR,
+            "ELECTRONS": {
+                "conv_thr": self._CONV_THR,
             },
         }
 
-        self.ctx.pw_parameters = update_dict(self.ctx.pw_parameters,
-                                        self.ctx.extra_parameters)
+        # TBD: Always use dual=8 since pseudo_O here is non-NC
+        self.ctx.ecutwfc = self._ECUTWFC
+        self.ctx.ecutrho = self._ECUTWFC * 8
+
+        self.ctx.pw_parameters = update_dict(self.ctx.pw_parameters, parameters)
 
         self.report(
             f'The pw parameters for convergence is: {self.ctx.pw_parameters}'
         )
 
-    def setup_code_resource_options(self):
+    def setup_pw_resource_options(self):
         """
         setup resource options and parallelization for `PwCalculation` from inputs
         """
-        if 'options' in self.inputs:
+        if "options" in self.inputs:
             self.ctx.options = self.inputs.options.get_dict()
         else:
             from aiida_sssp_workflow.utils import get_default_options
 
             self.ctx.options = get_default_options(
-                max_wallclock_seconds=self._MAX_WALLCLOCK_SECONDS,
-                with_mpi=True)
+                max_wallclock_seconds=self._MAX_WALLCLOCK_SECONDS, with_mpi=True
+            )
 
-        if 'parallelization' in self.inputs:
+        if "parallelization" in self.inputs:
             self.ctx.parallelization = self.inputs.parallelization.get_dict()
         else:
             self.ctx.parallelization = {}
 
-        self.report(f'resource options set to {self.ctx.options}')
-        self.report(
-            f'parallelization options set to {self.ctx.parallelization}')
+        self.report(f"resource options set to {self.ctx.options}")
+        self.report(f"parallelization options set to {self.ctx.parallelization}")
 
     def _get_inputs(self, element, pseudos):
         """
@@ -203,72 +187,40 @@ class BandsDistanceWorkChain(WorkChain):
         if element in RARE_EARTH_ELEMENTS:
             pseudos['N'] = self.ctx.pseudo_N
 
-        # yapf: disable
         inputs = {
-            'code': self.inputs.code,
+            'code': self.inputs.pw_code,
             'pseudos': pseudos,
             'structure': self.ctx.structure,
             'pw_base_parameters': orm.Dict(dict=self.ctx.pw_parameters),
             'ecutwfc': orm.Float(self.ctx.ecutwfc),
             'ecutrho': orm.Float(self.ctx.ecutrho),
             'kpoints_distance': orm.Float(self.ctx.kpoints_distance),
-            'init_nbands_factor': orm.Float(self._INIT_NBANDS_FACTOR),
+            'init_nbands_factor': orm.Float(self.ctx.init_nbands_factor),
+            'bands_shift': orm.Float(self.ctx.bands_shift),
+            'should_run_bands_structure': orm.Bool(True),
             'options': orm.Dict(dict=self.ctx.options),
             'parallelization': orm.Dict(dict=self.ctx.parallelization),
             'clean_workdir': orm.Bool(False),   # will leave the workdir clean to outer most wf
         }
-        # yapf: enable
 
         return inputs
 
     def run_bands_evaluation(self):
-        """run bands evaluation of pp in inputs list"""
-        for name, pseudo in self.inputs.input_pseudos.items():
-            pseudos = {self.ctx.element: pseudo}
-            inputs = self._get_inputs(self.ctx.element, pseudos)
+        """run bands evaluation of psp in inputs list"""
+        pseudos = {self.ctx.element: self.inputs.pseudo}
 
-            running = self.submit(BandsWorkChain, **inputs)
+        inputs = self._get_inputs(self.ctx.element, pseudos)
 
-            # set the name to indicate the pp
-            running.set_extra('name', name)
-            self.report(
-                f'launching pseudo >{name}<: {pseudo} BandsWorkChain<{running.pk}>'
-            )
+        running = self.submit(BandsWorkChain, **inputs)
 
-            self.to_context(children=append_(running))
+        self.report(
+            f'launching pseudo >_<: {self.inputs.pseudo} BandsWorkChain<{running.pk}>'
+        )
 
-    def calculate_bands_distance(self):
-        """calculate bands distance of every pair of pp"""
-        from itertools import combinations
+        return ToContext(bands=running)
 
-        children = self.ctx.children
-        success_children = [
-            child for child in children if child.is_finished_ok
-        ]
-
-        d_output_parameters = {}
-
-        for idx, (wca, wcb) in enumerate(combinations(success_children, 2)):
-            d_ab = {
-                'workchain_name_a': wca.extras['name'],
-                'workchain_name_b': wcb.extras['name']
-            }
-
-            wca_bands_parameters = wca.outputs.output_bands_parameters
-            wcb_bands_parameters = wcb.outputs.output_bands_parameters
-            wca_bands_structure = wca.outputs.output_bands_structure
-            wcb_bands_structure = wcb.outputs.output_bands_structure
-
-            res = calculate_bands_distance(
-                bands_structure_a=wca_bands_structure,
-                bands_parameters_a=wca_bands_parameters,
-                bands_structure_b=wcb_bands_structure,
-                bands_parameters_b=wcb_bands_parameters,
-                smearing=orm.Float(self.ctx.degauss * self._RY_TO_EV),
-                is_metal=orm.Bool(self.ctx.is_metal))
-
-            d_ab.update(res.get_dict())
-            d_output_parameters[f'dist_{idx}'] = d_ab
-
-        self.out('output_bands_distance',
-                 orm.Dict(dict=d_output_parameters).store())
+    def finalize(self):
+        """inspect bands run results"""
+        self.out_many(
+            self.exposed_outputs(self.ctx.bands, BandsWorkChain)
+        )
