a16b535b4f4a0785c84979c20505a2d9a790473e
Jusong Yu <jusong.yeu@gmail.com>
2022-03-27 10:13:55 +0200

    delta measure use _delta evaluate

--- examples/example_verification.py
+++ examples/example_verification.py
@@ -25,10 +25,10 @@ def run_verification(pw_code, ph_code, upf):
         "cutoff_control": orm.Str("test"),
         "properties_list": orm.List(
             list=[
-                # "accuracy:delta",
+                "accuracy:delta",
                 # "accuracy:bands_measure",
                 # "convergence:cohesive_energy",
-                "convergence:phonon_frequencies",
+                # "convergence:phonon_frequencies",
                 # "convergence:pressure",
                 # "convergence:delta",
                 # "convergence:bands_distance",
