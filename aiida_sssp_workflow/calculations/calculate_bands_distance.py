259d1f6320c6253ebcbb138c9821e4703b1892b6
Jusong Yu <jusong.yeu@gmail.com>
2022-05-26 02:33:56 +0200

    weights

--- aiida_sssp_workflow/calculations/calculate_bands_distance.py
+++ aiida_sssp_workflow/calculations/calculate_bands_distance.py
@@ -50,6 +50,7 @@ def retrieve_bands(
     I simply concatenate along the first dimension.
     """
     bands = bandsdata.get_bands()
+    kpoints, weights = bandsdata.get_kpoints(also_weights=True)
 
     # reduce by first dimension of up, down spins
     if len(bands.shape) > 2:
@@ -60,19 +61,19 @@ def retrieve_bands(
     bands = bands - efermi  # shift all bands to fermi energy 0
     bands = bands[:, start_band:]
     output_bands = orm.ArrayData()
-    output_bands.set_array("kpoints", bandsdata.get_kpoints())
+    output_bands.set_array("kpoints", kpoints)
+    output_bands.set_array("weights", weights)
     output_bands.set_array("bands", bands)
 
     if do_smearing:
         # for bands distance convergence
         # and metals in bands measure verification.
         nelectrons = num_electrons
-        nkpoints = np.shape(bands)[0]
-        weights = np.ones(nkpoints) / nkpoints
         bands = np.asfortranarray(bands)
         meth = 2  # firmi-dirac smearing
 
         output_efermi = find_efermi(bands, weights, nelectrons, smearing, meth)
+        print(output_efermi)
 
     else:
         # easy to spot the efermi energy only used for non-metals of typical configurations
@@ -101,8 +102,16 @@ def calculate_eta_and_max_diff(
 
     from scipy.optimize import minimize
 
+    weight_a = bands_a.get_array("weights")
+    weight_b = bands_b.get_array("weights")
+    weight = weight_a
+    assert np.allclose(
+        weight_a, weight_b
+    ), "Different weight of kpoints of two calculation."
+
     bands_a = bands_a.get_array("bands")
     bands_b = bands_b.get_array("bands")
+
     num_bands = min(np.shape(bands_a)[1], np.shape(bands_b)[1])
 
     assert np.shape(bands_a)[0] == np.shape(bands_b)[0], "have different kpoints"
@@ -118,7 +127,10 @@ def calculate_eta_and_max_diff(
     bands_diff = bands_a - bands_b
 
     def fun_shift(occ, bands_diff, shift):
-        return np.sqrt(np.sum(occ * (bands_diff + shift) ** 2) / np.sum(occ))
+        # import ipdb; ipdb.set_trace()
+        nominator = np.multiply(weight[:, None], (occ * (bands_diff + shift) ** 2))
+        denominator = np.multiply(weight[:, None], occ)
+        return np.sqrt(np.sum(nominator) / np.sum(denominator))
 
     # Compute eta
     eta_val = partial(fun_shift, occ, bands_diff)
@@ -156,34 +168,25 @@ def get_bands_distance(
     """
     num_electrons_a = band_parameters_a["number_of_electrons"]
     num_electrons_b = band_parameters_b["number_of_electrons"]
+
+    assert (
+        not num_electrons_a > num_electrons_b
+    ), "Need to be less num_electrons result as argument labeled 'a'."
+
     efermi_a = band_parameters_a["fermi_energy"]
     efermi_b = band_parameters_b["fermi_energy"]
 
-    if num_electrons_a <= num_electrons_b:
-        num_electrons = int(num_electrons_a)
-        res = retrieve_bands(bands_a, 0, num_electrons, efermi_a, smearing, do_smearing)
-        bands_a = res["bands"]
-        efermi_a = res["efermi"]
+    num_electrons = int(num_electrons_a)
+    res = retrieve_bands(bands_a, 0, num_electrons, efermi_a, smearing, do_smearing)
+    bands_a = res["bands"]
+    efermi_a = res["efermi"]
 
-        start_band = int(num_electrons_b - num_electrons_a) // 2
-        res = retrieve_bands(
-            bands_b, start_band, num_electrons, efermi_b, smearing, do_smearing
-        )
-        bands_b = res["bands"]
-        efermi_b = res["efermi"]
-    else:
-        # num_electrons_b < num_electrons_a:
-        num_electrons = int(num_electrons_b)
-        start_band = int(num_electrons_a - num_electrons_b) // 2
-        res = retrieve_bands(
-            bands_a, start_band, num_electrons, efermi_a, smearing, do_smearing
-        )
-        bands_a = res["bands"]
-        efermi_a = res["efermi"]
-
-        res = retrieve_bands(bands_b, 0, num_electrons, efermi_b, smearing, do_smearing)
-        bands_b = res["bands"]
-        efermi_b = res["efermi"]
+    start_band = int(num_electrons_b - num_electrons_a) // 2
+    res = retrieve_bands(
+        bands_b, start_band, num_electrons, efermi_b, smearing, do_smearing
+    )
+    bands_b = res["bands"]
+    efermi_b = res["efermi"]
 
     # eta_v
     fermi_shift_v = 0.0
@@ -192,6 +195,7 @@ def get_bands_distance(
     else:
         smearing_v = 0
 
+    # import ipdb; ipdb.set_trace()
     outputs = calculate_eta_and_max_diff(
         bands_a, bands_b, efermi_a, efermi_b, fermi_shift_v, smearing_v
     )
