1d5e44a12daf56926f63e5c3e8b1a6c6278f1eaf
Jusong Yu <jusong.yeu@gmail.com>
2022-11-20 17:57:02 +0100

    Print failed calc

--- tests/test_nightly.py
+++ tests/test_nightly.py
@@ -6,125 +6,107 @@ from aiida.plugins import WorkflowFactory
 VerificationWorkChain = WorkflowFactory("sssp_workflow.verification")
 
 
+@pytest.fixture(scope="function")
+def generate_test_inputs(pw_code, ph_code, psp_Si_SG15):
+    def _generate_inputs(properties_list):
+        inputs = {
+            "accuracy": {
+                "protocol": orm.Str("test"),
+                "cutoff_control": orm.Str("test"),
+            },
+            "convergence": {
+                "protocol": orm.Str("test"),
+                "cutoff_control": orm.Str("test"),
+                "criteria": orm.Str("efficiency"),
+            },
+            "pw_code": pw_code,
+            "ph_code": ph_code,
+            "pseudo": psp_Si_SG15,
+            "properties_list": orm.List(list=properties_list),
+            "label": orm.Str("sg15/Si_ONCV_PBE-1.2.upf"),
+            "options": orm.Dict(
+                dict={
+                    "resources": {
+                        "num_machines": 1,
+                        "num_mpiprocs_per_machine": 2,
+                    },
+                    "max_wallclock_seconds": 1800 * 3,
+                    "withmpi": False,
+                }
+            ),
+            "parallelization": orm.Dict(dict={"npool": 1}),
+            "clean_workchain": orm.Bool(False),
+        }
+
+        return inputs
+
+    return _generate_inputs
+
+
 @pytest.mark.usefixtures("aiida_profile_clean")
-def test_dummy(aiida_localhost):
+def test_verification_accuracy_workflow(generate_test_inputs, data_regression):
     """Test nightly."""
-    aiida_localhost.set_use_double_quotes(True)
-    engine_command = """singularity exec --bind $PWD:$PWD {image_name}"""
-    containerized_code = orm.ContainerizedCode(
-        default_calc_job_plugin="core.arithmetic.add",
-        filepath_executable="/bin/sh",
-        engine_command=engine_command,
-        image_name="docker://alpine:3",
-        computer=aiida_localhost,
-    ).store()
-    builder = containerized_code.get_builder()
-    builder.x = orm.Int(4)
-    builder.y = orm.Int(6)
-    builder.metadata.options.resources = {
-        "num_machines": 1,
-        "num_mpiprocs_per_machine": 1,
-    }
 
-    results, node = launch.run_get_node(builder)
-
-    print(results, node)
-
-
-@pytest.mark.usefixtures("aiida_profile_clean")
-def test_verification_accuracy_workflow(psp_Si_SG15, pw_code, ph_code, data_regression):
-    """Test nightly."""
+    from aiida import orm
 
     properties_list = [
         "accuracy.delta",
-        "accuracy.bands",
     ]
 
-    inputs = {
-        "accuracy": {
-            "protocol": orm.Str("test"),
-            "cutoff_control": orm.Str("test"),
-        },
-        "convergence": {
-            "protocol": orm.Str("test"),
-            "cutoff_control": orm.Str("test"),
-            "criteria": orm.Str("efficiency"),
-        },
-        "pw_code": pw_code,
-        "ph_code": ph_code,
-        "pseudo": psp_Si_SG15,
-        "properties_list": orm.List(list=properties_list),
-        "label": orm.Str("sg15/Si_ONCV_PBE-1.2.upf"),
-        "options": orm.Dict(
-            dict={
-                "resources": {
-                    "num_machines": 1,
-                    "num_mpiprocs_per_machine": 2,
-                },
-                "max_wallclock_seconds": 1800 * 3,
-                "withmpi": False,
-            }
-        ),
-        "parallelization": orm.Dict(dict={"npool": 1}),
-        "clean_workchain": orm.Bool(True),
-    }
-
+    inputs = generate_test_inputs(properties_list)
     _, node = launch.run_get_node(VerificationWorkChain, **inputs)
 
-    assert "bands" in node.outputs.accuracy
+    qb = orm.QueryBuilder()
+    qb.append(
+        cls=orm.CalcJobNode,
+        filters={
+            "attributes.process_state": "finished",
+            "attributes.exit_status": 305,
+        },
+    )
+    fnode = qb.all()[0][0]
+
+    fnode.outputs.remote_folder.getfile("_scheduler-stderr.txt", "/tmp/err.txt")
+    with open("/tmp/err.txt", "r") as fh:
+        print(fh.read())
 
     data_regression.check(node.outputs.accuracy.delta.output_parameters.get_dict())
 
 
-# @pytest.mark.usefixtures("aiida_profile_clean")
-# def test_verification_convergence_workflow(
-#     psp_Si_SG15, pw_code, ph_code, data_regression
-# ):
-#     """Test nightly."""
-#     properties_list = [
-#         # "convergence.cohesive_energy",
-#         "convergence.phonon_frequencies",
-#         "convergence.pressure",
-#         "convergence.delta",
-#         "convergence.bands",
-#     ]
+@pytest.mark.usefixtures("aiida_profile_clean")
+def test_verification_accuracy_bands_workflow(generate_test_inputs, data_regression):
+    """Test nightly."""
 
-#     inputs = {
-#         "accuracy": {
-#             "protocol": orm.Str("test"),
-#             "cutoff_control": orm.Str("test"),
-#         },
-#         "convergence": {
-#             "protocol": orm.Str("test"),
-#             "cutoff_control": orm.Str("test"),
-#             "criteria": orm.Str("efficiency"),
-#         },
-#         "pw_code": pw_code,
-#         "ph_code": ph_code,
-#         "pseudo": psp_Si_SG15,
-#         "properties_list": orm.List(list=properties_list),
-#         "label": orm.Str("sg15/Si_ONCV_PBE-1.2.upf"),
-#         "options": orm.Dict(
-#             dict={
-#                 "resources": {
-#                     "num_machines": 1,
-#                     "num_mpiprocs_per_machine": 2,
-#                 },
-#                 "max_wallclock_seconds": 1800 * 3,
-#                 "withmpi": False,
-#             }
-#         ),
-#         "parallelization": orm.Dict(dict={"npool": 1}),
-#         "clean_workchain": orm.Bool(True),
-#     }
+    properties_list = [
+        "accuracy.bands",
+    ]
 
-#     _, node = launch.run_get_node(VerificationWorkChain, **inputs)
+    inputs = generate_test_inputs(properties_list)
+    _, node = launch.run_get_node(VerificationWorkChain, **inputs)
 
-#     data_regression.check(node.outputs.pseudo_info.get_dict())
-#     # data_regression.check(node.outputs.accuracy.delta.output_parameters.get_dict())
+    assert "bands" in node.outputs.accuracy
 
-#     assert "cohesive_energy" in node.outputs.convergence
-#     assert "phonon_frequencies" in node.outputs.convergence
-#     assert "pressure" in node.outputs.convergence
-#     assert "delta" in node.outputs.convergence
-#     assert "bands" in node.outputs.convergence
+
+@pytest.mark.usefixtures("aiida_profile_clean")
+def test_verification_convergence_workflow(generate_test_inputs, data_regression):
+    """Test nightly."""
+    properties_list = [
+        "convergence.cohesive_energy",
+        "convergence.phonon_frequencies",
+        "convergence.pressure",
+        "convergence.delta",
+        "convergence.bands",
+    ]
+
+    inputs = generate_test_inputs(properties_list)
+
+    _, node = launch.run_get_node(VerificationWorkChain, **inputs)
+
+    data_regression.check(node.outputs.pseudo_info.get_dict())
+    # data_regression.check(node.outputs.accuracy.delta.output_parameters.get_dict())
+
+    assert "cohesive_energy" in node.outputs.convergence
+    assert "phonon_frequencies" in node.outputs.convergence
+    assert "pressure" in node.outputs.convergence
+    assert "delta" in node.outputs.convergence
+    assert "bands" in node.outputs.convergence
