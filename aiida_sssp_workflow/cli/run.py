c684c1d5426eb619b34f059d4b5458407526ff13
Jusong Yu <jusong.yeu@gmail.com>
2023-02-24 17:27:00 +0100

    CLI: aiida-sssp-workflow launch to run the verification (#173)
    * Add protocol for OPSP verification
    
    * Add protocal for opsp quick verification
    
    * Renew the CLI

--- aiida_sssp_workflow/cli/run.py
+++ aiida_sssp_workflow/cli/run.py
@@ -8,150 +8,138 @@ import os
 import aiida
 import click
 from aiida import orm
+from aiida.cmdline.params import options, types
 from aiida.engine import run_get_node, submit
 from aiida.plugins import DataFactory, WorkflowFactory
 
 from aiida_sssp_workflow.cli import cmd_root
-from aiida_sssp_workflow.workflows.verifications import (
-    DEFAULT_CONVERGENCE_PROPERTIES_LIST,
-    DEFAULT_PROPERTIES_LIST,
-)
+from aiida_sssp_workflow.workflows.verifications import DEFAULT_PROPERTIES_LIST
 
 UpfData = DataFactory("pseudo.upf")
 VerificationWorkChain = WorkflowFactory("sssp_workflow.verification")
 
 SSSP_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "_sssp")
 
+# Trigger the launch by running:
+# aiida-sssp-workflow launch --property accuracy.delta --pw-code pw-7.0@localhost --ph-code ph-7.0@localhost --protocol test --cutoff-control test --criteria efficiency --withmpi True -- examples/_static/Si_ONCV_PBE-1.2.upf
+
 
 @cmd_root.command("launch")
+@options.OverridableOption(
+    "--pw-code", "pw_code", type=types.CodeParamType(entry_point="quantumespresso.pw")
+)(required=True)
+@options.OverridableOption(
+    "--ph-code", "ph_code", type=types.CodeParamType(entry_point="quantumespresso.ph")
+)(required=True)
 @click.option(
-    "--mode",
-    type=click.Choice(["TEST", "PRECHECK", "STANDARD"], case_sensitive=False),
-    help="mode of verification.",
+    "--property",
+    multiple=True,
+    default=[],
+    help="Property to verify, can be: accuracy.delta, accuracy.bands, convergence ...",
 )
 @click.option(
-    "--computer", type=str, help="computer (aiida) to run non-test verification."
+    "protocol",
+    "--protocol",
+    default="standard",
+    help="Protocol to use for the verification.",
 )
 @click.option(
-    "--cleanup/--no-cleanup",
+    "cutoff_control",
+    "--cutoff-control",
+    default="standard",
+    help="Control of convergence.",
+)
+@click.option(
+    "criteria", "--criteria", default="efficiency", help="Criteria of convergence."
+)
+@click.option("withmpi", "--withmpi", default=True, help="Run with mpi.")
+@click.option("npool", "--npool", default=1, help="Number of pool.")
+@click.option("walltime", "--walltime", default=3600, help="Walltime.")
+@click.option("num_mpiprocs", "--num-mpiprocs", default=1, help="Number of mpiprocs.")
+@click.option(
+    "--clean-workchain/--no-clean-workchain",
     default=True,
     help="Clean up the remote folder of all calculation, turn this off when your want to see the remote for details.",
 )
-@click.option("--property", multiple=True, default=[])
-@click.argument("filename", type=click.Path(exists=True))
-def launch(mode, filename, computer, property, cleanup):
+@click.option(
+    "--daemon/--no-daemon",
+    default=True,
+    help="Launch the verfication to daemon by submit or run directly.",
+)
+@click.argument("pseudo", type=click.Path(exists=True))
+def launch(
+    pw_code,
+    ph_code,
+    property,
+    protocol,
+    cutoff_control,
+    criteria,
+    withmpi,
+    npool,
+    walltime,
+    num_mpiprocs,
+    pseudo,
+    clean_workchain,
+    daemon,
+):
+    """Launch the verification workchain."""
+    from aiida.orm import load_code
+
+    # if the property is not specified, use the default list with all properties calculated.
+    # otherwise, use the specified properties.
     if not property:
-        extra_desc = "all_prop"
-        if mode == "PRECHECK":
-            properties_list = DEFAULT_CONVERGENCE_PROPERTIES_LIST
-        else:
-            properties_list = DEFAULT_PROPERTIES_LIST
+        properties_list = DEFAULT_PROPERTIES_LIST
     else:
         properties_list = list(property)
         extra_desc = f"{properties_list}"
 
     _profile = aiida.load_profile()
-    click.echo(f"Profile: {_profile.name}")
+    click.echo(f"Current profile: {_profile.name}")
 
-    inputs = inputs_from_mode(
-        mode=mode, computer_label=computer, properties_list=properties_list
-    )
+    basename = os.path.basename(pseudo)
 
-    inputs["clean_workchain"] = cleanup
-
-    basename = os.path.basename(filename)
+    computer = pw_code.computer.label
     label, _ = os.path.splitext(basename)
-    label = orm.Str(f"({mode}-{computer}) {label}")
+    label = orm.Str(f"({protocol}-{criteria}-{cutoff_control} at {computer}) {label}")
 
-    with open(filename, "rb") as stream:
+    with open(pseudo, "rb") as stream:
         pseudo = UpfData(stream)
 
+    _basic_inputs = {
+        "pw_code": pw_code,
+        "ph_code": ph_code,
+        "protocol": orm.Str(protocol),
+        "cutoff_control": orm.Str(cutoff_control),
+        "criteria": orm.Str(criteria),
+        "options": orm.Dict(
+            dict={
+                "resources": {
+                    "num_machines": 1,
+                    "num_mpiprocs_per_machine": num_mpiprocs,
+                },
+                "max_wallclock_seconds": walltime,
+                "withmpi": withmpi,
+            }
+        ),
+        "parallization": orm.Dict(dict={"npool": npool}),
+        "properties_list": orm.List(list=properties_list),
+    }
+
+    _pseudo_inputs = {
+        "pseudo": pseudo,
+        "label": label,
+        "extra_desc": extra_desc,
+    }
+
     node = run_verification(
-        **inputs,
-        **{
-            "pseudo": pseudo,
-            "label": label,
-            "extra_desc": extra_desc,
-        },
+        **_basic_inputs,
+        **_pseudo_inputs,
+        clean_workchain=clean_workchain,
+        on_daemon=daemon,
     )
 
     click.echo(node)
     click.echo(f"calculated on property: {'/'.join(properties_list)}")
-
-
-def inputs_from_mode(mode, computer_label, properties_list):
-    if "imx" in computer_label:
-        mpiprocs = 32
-        npool = 4
-        walltime = 3600
-    elif "eiger-mc" in computer_label:
-        mpiprocs = 128
-        npool = 16
-        walltime = 1800
-    elif "daint-mc" in computer_label:
-        mpiprocs = 36
-        npool = 4
-        walltime = 3600
-
-    inputs = {}
-    if mode == "TEST":
-        inputs["pw_code"] = orm.load_code("pw-7.0@localhost")
-        inputs["ph_code"] = orm.load_code("ph-7.0@localhost")
-        inputs["protocol"] = orm.Str("test")
-        inputs["cutoff_control"] = orm.Str("test")
-        inputs["criteria"] = orm.Str("efficiency")
-        inputs["options"] = orm.Dict(
-            dict={
-                "resources": {
-                    "num_machines": 1,
-                    "num_mpiprocs_per_machine": 1,
-                },
-                "max_wallclock_seconds": 1800,
-                "withmpi": False,
-            }
-        )
-        inputs["parallization"] = orm.Dict(dict={})
-        inputs["properties_list"] = orm.List(list=properties_list)
-
-    if mode == "PRECHECK":
-        inputs["pw_code"] = orm.load_code(f"pw-7.0@{computer_label}")
-        inputs["ph_code"] = orm.load_code(f"ph-7.0@{computer_label}")
-        inputs["protocol"] = orm.Str("acwf")
-        inputs["cutoff_control"] = orm.Str("precheck")
-        inputs["criteria"] = orm.Str("precision")
-        inputs["options"] = orm.Dict(
-            dict={
-                "resources": {
-                    "num_machines": 1,
-                    "num_mpiprocs_per_machine": mpiprocs,
-                },
-                "max_wallclock_seconds": walltime,
-                "withmpi": True,
-            }
-        )
-        inputs["parallization"] = orm.Dict(dict={"npool": npool})
-        inputs["properties_list"] = orm.List(list=properties_list)
-
-    if mode == "STANDARD":
-        inputs["pw_code"] = orm.load_code(f"pw-7.0@{computer_label}")
-        inputs["ph_code"] = orm.load_code(f"ph-7.0@{computer_label}")
-        inputs["protocol"] = orm.Str("acwf")
-        inputs["cutoff_control"] = orm.Str("standard")
-        inputs["criteria"] = orm.Str("efficiency")
-        inputs["options"] = orm.Dict(
-            dict={
-                "resources": {
-                    "num_machines": 1,
-                    "num_mpiprocs_per_machine": mpiprocs,
-                },
-                "max_wallclock_seconds": walltime,
-                "withmpi": True,
-            }
-        )
-        inputs["parallization"] = orm.Dict(dict={"npool": npool})
-        inputs["properties_list"] = orm.List(list=properties_list)
-
-    return inputs
 
 
 def run_verification(
@@ -167,6 +155,7 @@ def run_verification(
     label,
     extra_desc,
     clean_workchain,
+    on_daemon,
 ):
     """
     pw_code: code for pw.x calculation
@@ -199,10 +188,10 @@ def run_verification(
         "clean_workchain": orm.Bool(clean_workchain),
     }
 
-    if cutoff_control.value == "test":
-        _, node = run_get_node(VerificationWorkChain, **inputs)
-    else:
+    if on_daemon:
         node = submit(VerificationWorkChain, **inputs)
+    else:
+        _, node = run_get_node(VerificationWorkChain, **inputs)
 
     node.description = f"{label.value} ({extra_desc})"
 
