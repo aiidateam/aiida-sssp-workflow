2f1470254b1487ba94a1e29da79c0e87468f885d
Jusong Yu <jusong.yeu@gmail.com>
2023-05-08 18:20:48 +0200

    dynamic set the configuration for delta convergence workflow

--- aiida_sssp_workflow/workflows/convergence/delta.py
+++ aiida_sssp_workflow/workflows/convergence/delta.py
@@ -2,16 +2,14 @@
 """
 Convergence test on cohesive energy of a given pseudopotential
 """
+import importlib
+import json
 
 from aiida import orm
 from aiida.engine import calcfunction
 from aiida.plugins import DataFactory
 
-from aiida_sssp_workflow.utils import (
-    ACTINIDE_ELEMENTS,
-    RARE_EARTH_ELEMENTS,
-    update_dict,
-)
+from aiida_sssp_workflow.utils import RARE_EARTH_ELEMENTS, update_dict
 from aiida_sssp_workflow.workflows.convergence._base import _BaseConvergenceWorkChain
 from aiida_sssp_workflow.workflows.evaluate._delta import DeltaWorkChain
 
@@ -77,13 +75,29 @@ class ConvergenceDeltaWorkChain(_BaseConvergenceWorkChain):
         self.ctx.scale_count = self._SCALE_COUNT = protocol["scale_count"]
         self.ctx.scale_increment = self._SCALE_INCREMENT = protocol["scale_increment"]
 
-        # configuration for delta convergence
-        if self.ctx.element in RARE_EARTH_ELEMENTS:
-            self.ctx.configuration = "RE"
-        if self.ctx.element in ACTINIDE_ELEMENTS:
-            self.ctx.configuration = "FCC"
-        else:
+        # configuration for delta convergence read from mapping.json of static
+        # Instead of hard code the configuration, the configuration is read from mapping.json
+        import_path = importlib.resources.path(
+            "aiida_sssp_workflow.statics.structures", f"mapping.json"
+        )
+        with import_path as path, open(path, "r") as handle:
+            mapping = json.load(handle)
+
+        dir, fn = mapping[self.ctx.element]["convergence"].split("/")
+
+        if str.upper(dir) == "TYPICAL":
+            self.report(f"Use typical configuration for {self.ctx.element}")
+            element = fn.split(".")[0]
+            assert element == self.ctx.element
             self.ctx.configuration = "TYPICAL"
+        else:
+            # the convergence configuration is read from ACWF set
+            element, configuration = fn.replace("-", ".").split(".")[:-1]
+            self.report(
+                f"Use ACWF configuration {configuration} for {self.ctx.element}"
+            )
+            assert element == self.ctx.element
+            self.ctx.configuration = configuration
 
         # Set context parameters
         self.ctx.kpoints_distance = self._KDISTANCE
