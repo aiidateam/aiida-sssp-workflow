155c23175410ab9669cd8fd666ceabf6e38b1fab
Jusong Yu <jusong.yeu@gmail.com>
2023-08-09 14:48:46 +0200

    Re-factoring the bands workchain nfactor increase logic

--- aiida_sssp_workflow/workflows/evaluate/_bands.py
+++ aiida_sssp_workflow/workflows/evaluate/_bands.py
@@ -52,7 +52,7 @@ def create_kpoints_from_distance(structure, distance, force_parity):
 
 def validate_inputs(inputs, ctx=None):
     """Validate the inputs of the entire input namespace"""
-    if inputs["run_bands_structure"] and (
+    if inputs["run_band_structure"] and (
         "kpoints_distance_band_structure" not in inputs
     ):
         return (
@@ -76,11 +76,11 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
         spec.expose_inputs(PwBandsWorkChain, include=['scf', 'bands', 'structure'])
         spec.input('kpoints_distance_bands', valid_type=orm.Float,
                     help='Kpoints distance setting for bulk energy bands calculation.')
-        spec.input('init_nbands_factor', valid_type=orm.Float,
+        spec.input('init_nbands_factor', valid_type=orm.Float, default=lambda: orm.Float(1.5),
                     help='initial nbands factor.')
         spec.input('fermi_shift', valid_type=orm.Float, default=lambda: orm.Float(10.0),
                     help='The uplimit of energy to check the bands diff, control the number of bands.')
-        spec.input('run_bands_structure', valid_type=orm.Bool, default=lambda: orm.Bool(False),
+        spec.input('run_band_structure', valid_type=orm.Bool, default=lambda: orm.Bool(False),
                     help='if True, run bands structure calculation on seekpath kpath.')
         spec.input('kpoints_distance_band_structure', valid_type=orm.Float, required=False,
                     help='Kpoints distance setting for band structure calculation.')
@@ -89,13 +89,13 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
         spec.outline(
             cls.setup,
 
-            cls.run_bands,
-            while_(cls.not_enough_bands)(
-                cls.increase_nbands,
+            while_(cls.should_run_bands)(
                 cls.run_bands,
+                cls.inspect_bands,
+                cls.increase_nbands,
             ),
 
-            if_(cls.run_bands_structure)(
+            if_(cls.should_run_band_structure)(
                 cls.run_band_structure,
                 cls.inspect_band_structure,
             ),
@@ -109,7 +109,7 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
                     message='The `PwBandsWorkChain` sub process failed.')
         spec.exit_code(203, 'ERROR_REACH_MAX_BANDS_FACTOR_INCREASE_LOOP',
                     message=f'The maximum number={cls._MAX_NUM_BANDS_FACTOR} of bands factor'
-                            'increase loop reached.')
+                            'increase loop reached, but still cannot get enough bands.')
         spec.exit_code(204, 'ERROR_KPOINTS_DISTANCE_BAND_STRUCTURE_NOT_SET',
                     message=f'kpoints distance is not set in inputs.')
         # yapf: enable
@@ -117,8 +117,7 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
     def setup(self):
         """Input validation"""
         # set initial lowest highest bands eigenvalue - fermi_energy equals to 0.0
-        self.ctx.nbands_factor = self.inputs.init_nbands_factor
-        self.ctx.lowest_highest_eigenvalue = 0.0
+        self.ctx.nbands_factor = self.inputs.init_nbands_factor.value
 
         # For qe PwBandsWorkChain if `bands_kpoints` not set, the seekpath will run
         # to give a seekpath along the recommonded path.
@@ -133,20 +132,24 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
         # break_increase_nbands set to True will force the while_ loop to stop.
         self.ctx.break_increase_nbands = False
 
+        # flag to control whether bands loop keep on running
+        self.ctx.should_run_bands = True
+
+    def should_run_bands(self) -> bool:
+        return self.ctx.should_run_bands
+
     def run_bands(self):
         """run bands calculation"""
         inputs = self.exposed_inputs(PwBandsWorkChain)
-        inputs["nbands_factor"] = self.ctx.nbands_factor
+        inputs["nbands_factor"] = orm.Float(self.ctx.nbands_factor)
         inputs["bands_kpoints"] = self.ctx.bands_kpoints
 
         running = self.submit(PwBandsWorkChain, **inputs)
         self.report(f"Running pw bands calculation pk={running.pk}")
         return ToContext(workchain_bands=running)
 
-    def _inspect_bands(self):
-        """this is called from `not_enough_bands`
-        separated for the name is more readable.
-
+    def inspect_bands(self):
+        """
         Return workchain if it is finished_ok
         """
         workchain = self.ctx.workchain_bands
@@ -155,63 +158,65 @@ class BandsWorkChain(_BaseEvaluateWorkChain):
             self.logger.warning(
                 f"PwBandsWorkChain for bands evaluation failed with exit status {workchain.exit_status}"
             )
-            self.ctx.break_increase_nbands = True
 
             return self.exit_codes.ERROR_SUB_PROCESS_FAILED_BANDS
 
         self._disable_cache(workchain)
+
+        # Check if the bands are enough to cover the desired fermi shift
+        fermi_energy = workchain.outputs.band_parameters["fermi_energy"]
+        bands = workchain.outputs.band_structure.get_array("bands")
+
+        # -1 colume for the highest eigenvalue of every kpoints
+        # which might not be belong to one band if there are degeneracy
+        # not enough until eigenvalues of all kpoints are greater than shift value.
+        highest_band = bands[:, -1]
+
+        if np.all(highest_band > fermi_energy + self.inputs.fermi_shift.value):
+            self.ctx.should_run_bands = False
+        else:
+            self.report(
+                f"highest band eigenvalue above fermi is {min(highest_band) - fermi_energy}, not enough, you want {self.inputs.fermi_shift.value}."
+            )
 
         self.ctx.ecutwfc = workchain.inputs.scf.pw.parameters["SYSTEM"]["ecutwfc"]
         self.ctx.ecutrho = workchain.inputs.scf.pw.parameters["SYSTEM"]["ecutrho"]
 
         return workchain
 
-    def not_enough_bands(self) -> bool:
-        """inspect and check if the number of bands enough for fermi shift (_FERMI_SHIFT)
-        this is a if-else statement"""
-        if self.ctx.break_increase_nbands:
-            return False
-
-        inspect_res = self._inspect_bands()
-        if isinstance(inspect_res, ExitCode):
-            return inspect_res
-        else:
-            workchain = inspect_res
-
-        fermi_energy = workchain.outputs.band_parameters["fermi_energy"]
-        bands = workchain.outputs.band_structure.get_array("bands")
-        # -1 colume for the highest eigenvalue of every kpoints
-        # which might not be belong to one band if there are degeneracy
-        # not enough until eigenvalues of all kpoints are greater than shift value.
-        highest_band = bands[:, -1]
-
-        return bool(np.all(highest_band < fermi_energy + self.inputs.fermi_shift.value))
-
     def increase_nbands(self):
         """inspect the result of bands calculation."""
+
         # It is here not only because we don't want to stop bands number increase,
         # but also because of a bug that return exit in `while_` not terminate the workflow.
         #
         # If the bands evaluation failed it will keeps on increasing the `nbands_factor`
         # which lead to the infinite loop to this work chain.
         # Here I work around it by giving maximum nbands_factor loop to _MAX_NUM_BANDS_FACTOR=5.
-        # And add a `break_increase_nbands` to break and get out from `not_enough_bands` immediately.
+        # And add a `break_increase_nbands` to break and get out from `should_run_bands` immediately.
         if (
             self.ctx.nbands_factor
-            > self.inputs.init_nbands_factor + self._MAX_NUM_BANDS_FACTOR
+            > self.inputs.init_nbands_factor.value + self._MAX_NUM_BANDS_FACTOR
         ):
             return self.exit_codes.ERROR_REACH_MAX_BANDS_FACTOR_INCREASE_LOOP
 
-        self.ctx.nbands_factor += 1.0
+        if self.ctx.should_run_bands:
+            # only when bands is not enough, increase the factor and rerun
+            # I should not increase it anyway because this ctx is used
+            # for the input of band structure calculation.
+            self.report(
+                f"increasing nbands_factor to from {self.ctx.nbands_factor} to {self.ctx.nbands_factor + 1.0}"
+            )
+            self.ctx.nbands_factor += 1.0
 
-    def run_bands_structure(self):
+    def should_run_band_structure(self):
         """whether run band structure"""
-        return self.inputs.run_bands_structure.value
+        return self.inputs.run_band_structure.value
 
     def run_band_structure(self):
         """run band structure calculation"""
         inputs = self.exposed_inputs(PwBandsWorkChain)
-        inputs["nbands_factor"] = self.ctx.nbands_factor
+        inputs["nbands_factor"] = orm.Float(self.ctx.nbands_factor)
         inputs["bands_kpoints_distance"] = orm.Float(
             self.inputs.kpoints_distance_band_structure
         )
