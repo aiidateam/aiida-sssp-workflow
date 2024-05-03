8c3f6768c6d7f3ff8748f823bd1a90f88c783580
Jusong Yu <jusong.yeu@gmail.com>
2024-05-03 14:34:53 +0200

    Add more source to test pseudo info extraction

--- tests/utils/test_pseudo.py
+++ tests/utils/test_pseudo.py
@@ -11,15 +11,15 @@ upf_files = list(upf_folder.glob("*.upf"))
 
 
 @pytest.mark.parametrize(
-    "files",
+    "file",
     upf_files,
 )
-def test_extract_pseudo_info(files):
+def test_extract_pseudo_info(file):
     """Test the ``extract_pseudo_info`` function."""
-    with open(files, "r") as fh:
+    with open(file, "r") as fh:
         pseudo_text = fh.read()
 
     info_from_text = extract_pseudo_info(pseudo_text)
-    info_from_filename = parse_std_filename(files.name)
+    info_from_filename = parse_std_filename(file.name)
 
     assert info_from_text == info_from_filename
